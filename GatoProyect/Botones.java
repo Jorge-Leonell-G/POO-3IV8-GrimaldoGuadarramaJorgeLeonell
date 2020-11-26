61f235ba65bbbc93f54bb377f007b38d61bcfda6
Jorge-Leonell-G <jgrimaldog1900@alumno.ipn.mx>
2020-11-26 12:43:34 -0600

    La que iba a ser la clase para los Botones...

--- GatoProyect/Botones.java
+++ GatoProyect/Botones.java
@@ -2,6 +2,8 @@
 import javax.swing.JFrame;
 import javax.swing.JButton;
 
+//ESTA IBA A SER LA CLASE PARA MOSTRAR LOS BOTONES POR MEDIO DE JBUTTON...
+
 //utilizamos la palabra reservada extends para poder heredar las funciones de la super clase o en este caso
 //de JFrame 
 public class Botones extends JFrame{
