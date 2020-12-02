989bd56f4caa1164b4f7b4ad56bb325971fff90d
Jorge-Leonell-G <jgrimaldog1900@alumno.ipn.mx>
2020-12-01 21:11:05 -0600

    Mas ejercicios con cadenas y arrays

---
+++ Cadenas/Principal.java
@@ -0,0 +1,17 @@
+//este es el principal wiiii
+
+class Principal{
+
+    //main
+    public static void main(String[] args){
+
+        Funciones fun = new Funciones();
+        //fun.cadenas();
+
+        cadenaInvertida inver = new cadenaInvertida();
+        //inver.cadenaInvertida();
+
+        cadenaFunciones cadfun = new cadenaFunciones();
+        cadfun.masFuncionesCadena();
+    }
+}
